a579f757db96d05d42765e7476fcf56b5000f77a
Klaven Jones <klavensjones@gmail.com>
2023-04-01 21:35:49 -0400

    Add Logout

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,21 +1,27 @@
-import { LinksFunction } from '@remix-run/node';
+import type { User } from '@prisma/client';
+import type { LinksFunction, LoaderArgs } from '@remix-run/node';
 import { json } from '@remix-run/node';
 import { Link, Outlet, useLoaderData } from '@remix-run/react';
 
-import stylesUrl from '~/styles/jokes.css';
 import { db } from '~/utils/db.server';
+import { getUser } from '~/utils/session.server';
+import stylesUrl from '~/styles/jokes.css';
 
 export const links: LinksFunction = () => {
   return [{ rel: 'stylesheet', href: stylesUrl }];
 };
 
-export const loader = async () => {
+export const loader = async ({ request }: LoaderArgs) => {
+  const jokeListItems = await db.joke.findMany({
+    take: 5,
+    orderBy: { createdAt: 'desc' },
+    select: { id: true, name: true }
+  });
+  const user = await getUser(request);
+
   return json({
-    jokeListItems: await db.joke.findMany({
-      take: 5,
-      select: { id: true, name: true },
-      orderBy: { createdAt: 'desc' }
-    })
+    jokeListItems,
+    user
   });
 };
 
@@ -32,6 +38,18 @@ export default function JokesRoute() {
               <span className="logo-medium">J🤪KES</span>
             </Link>
           </h1>
+          {data.user ? (
+            <div className="user-info">
+              <span>{`Hi ${data.user.username}`}</span>
+              <form action="/logout" method="post">
+                <button type="submit" className="button">
+                  Logout
+                </button>
+              </form>
+            </div>
+          ) : (
+            <Link to="/login">Login</Link>
+          )}
         </div>
       </header>
       <main className="jokes-main">
@@ -39,7 +57,7 @@ export default function JokesRoute() {
           <div className="jokes-list">
             <Link to=".">Get a random joke</Link>
             <p>Here are a few more jokes to check out:</p>
-            <ul className="list-disc pl-4 my-4">
+            <ul className="list-disc my-4 pl-4">
               {data.jokeListItems.map((joke) => (
                 <li key={joke.id}>
                   <Link to={joke.id}>{joke.name}</Link>
